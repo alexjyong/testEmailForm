4ade343d9b7c8e423a109dc60a5dccca11e43fe9
Alex Yong <alexjyong@gmail.com>
2022-06-01 11:57:25 -0400

    Add files via upload

--- app.js
+++ app.js
@@ -33,36 +33,33 @@ console.log(`server is running at ${port}`);
 });
 
 async function main(fileName,theContent){
+// create reusable transporter object using the default SMTP transport
+let transporter = nodemailer.createTransport({
+	host: "smtp.ethereal.email",
+	port: 587,
+	secure: false, // true for 465, false for other ports
+	auth: {
+	  user: 'wgmizjlumtmxat26@ethereal.email',
+	  pass: 'zw5UnduYgEYpdYptWs'
+	},
+});
 
-	let testAccount = await nodemailer.createTestAccount();
-	// create reusable transporter object using the default SMTP transport
-	let transporter = nodemailer.createTransport({
-		host: "smtp.ethereal.email",
-		port: 587,
-		secure: false, // true for 465, false for other ports
-		auth: {
-			user: testAccount.user, // generated ethereal user
-			pass: testAccount.pass, // generated ethereal password
-	  
-		},
-	});
-
-	// send mail with defined transport object
-	let info = await transporter.sendMail({
-		from: '"Fred Foo 👻" <foo@example.com>', // sender address
-		to: "bar@example.com, baz@example.com", // list of receivers
-		subject: "Hello ✔", // Subject line
-		text: "Hello world?", // plain text body
-		html: "<b>Hello world?</b>", // html body
-		attachments: [
-		{   // encoded string as an attachment
-				filename: fileName,
-				path: theContent
-				//content: theContent,
-				//encoding: 'base64'
-			}
-		]
-	});
+// send mail with defined transport object
+let info = await transporter.sendMail({
+	from: '"Fred Foo 👻" <foo@example.com>', // sender address
+	to: "bar@example.com, baz@example.com", // list of receivers
+	subject: "Hello ✔", // Subject line
+	text: "Hello world?", // plain text body
+	html: "<b>Hello world?</b>", // html body
+	attachments: [
+	{   // encoded string as an attachment
+			filename: fileName,
+			path: theContent
+			//content: theContent,
+			//encoding: 'base64'
+		}
+	]
+});
 
 	console.log("Message sent: %s", info.messageId);
 	// Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
